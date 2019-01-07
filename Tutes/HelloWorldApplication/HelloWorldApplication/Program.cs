a377d93e13f30d6ebb39f62993be31f38a5d99ca
Elliot Schot <s3530160@student.rmit.edu.au>
2019-01-07 12:56:34 +1100

    Added HelloWorldApplication

---
+++ Tutes/HelloWorldApplication/HelloWorldApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
