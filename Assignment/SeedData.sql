58466df72dde25147723f6a25acd7a6909d0caab
Elliot Schot <s3530160@student.rmit.edu.au>
2019-01-07 15:56:08 +1100

    adding A1 files

---
+++ Assignment/SeedData.sql
@@ -0,0 +1,10 @@
+insert into Room (RoomID) values ('A');
+insert into Room (RoomID) values ('B');
+insert into Room (RoomID) values ('C');
+insert into Room (RoomID) values ('D');
+
+insert into [User] (UserID, Name, Email) values ('e12345', 'Matt', 'e12345@rmit.edu.au');
+insert into [User] (UserID, Name, Email) values ('e56789', 'Joe', 'e56789@rmit.edu.au');
+
+insert into [User] (UserID, Name, Email) values ('s1234567', 'Kevin', 's1234567@student.rmit.edu.au');
+insert into [User] (UserID, Name, Email) values ('s4567890', 'Olivier', 's4567890@student.rmit.edu.au');
