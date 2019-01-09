5548a665ba3516c67d7f8262aa120ca187fb632d
Elliot Schot <s3530160@student.rmit.edu.au>
2019-01-09 11:21:12 +1100

    Assignment 1 initial start + Main Menu

---
+++ ASR-System/ASR-System/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ASR_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                ShowMainMenu();
+            }
+            
+        }
+
+        static void ShowMainMenu()
+        {
+            Console.WriteLine("{0}", "Main menu:");
+            Console.WriteLine("{0}. {1}", 1, "List rooms");
+            Console.WriteLine("{0}. {1}", 2, "List slots");
+            Console.WriteLine("{0}. {1}", 3, "Staff menu");
+            Console.WriteLine("{0}. {1}", 4, "Student menu");
+            Console.WriteLine("{0}. {1}", 5, "Exit");
+            Console.WriteLine();
+
+            Console.Write("{0}", "Enter option: ");
+            Console.ReadLine();
+            Console.WriteLine();
+        }
+    }
+}
