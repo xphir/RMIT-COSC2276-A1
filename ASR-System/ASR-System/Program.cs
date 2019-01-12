edb1d3cabac5694b4b9b4e0dc014395a110ec49d
Elliot Schot <s3530160@student.rmit.edu.au>
2019-01-12 13:00:32 +1100

    adding DataValidation

--- ASR-System/ASR-System/Program.cs
+++ ASR-System/ASR-System/Program.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
+
 namespace ASR_System
 {
     class Program
     {
+        private static IConfigurationRoot Configuration { get; } = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+        public static string ConnectionString { get; } = Configuration["ConnectionString"];
+
         static void Main(string[] args)
         {
+            Utilities.DataValidation.testDataValidation();
             WelcomeMessage();
             MainMenu();
         }
@@ -255,6 +263,93 @@ namespace ASR_System
             string userInput = Console.ReadLine();
             Console.WriteLine();
             return userInput;
+        }
+
+
+    }
+}
+
+namespace Utilities
+{
+    public class DataValidation
+    {
+        //DATA VALIDATION
+        //e12345@rmit.edu.au
+        //s1234567@student.rmit.edu.au
+        public static void testDataValidation()
+        {
+            //STAFF ID
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("STARTING DATA VALIDATION CHECKS");
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("staffIdValidation TRUE 01: e12356 " + staffIdValidation("e12356"));
+            Console.WriteLine("staffIdValidation TRUE 02: e98765 " + staffIdValidation("e98765"));
+            Console.WriteLine("staffIdValidation FALSE 01: e1234 " + staffIdValidation("e1234"));
+            Console.WriteLine("staffIdValidation FALSE 02: s1234567 " + staffIdValidation("s1234567"));
+
+            //STUDENT ID
+            Console.WriteLine("studentIdValidation TRUE 01: s1234567 " + studentIdValidation("s1234567"));
+            Console.WriteLine("studentIdValidation TRUE 02: s3530160 " + studentIdValidation("s3530160"));
+            Console.WriteLine("studentIdValidation FALSE 01: s123456 " + studentIdValidation("s123456"));
+            Console.WriteLine("studentIdValidation FALSE 02: e12356 " + studentIdValidation("e12356"));
+
+            //STAFF EMAIL
+            Console.WriteLine("staffEmailValidation TRUE 01: e12345@rmit.edu.au " + staffEmailValidation("e12345@rmit.edu.au"));
+            Console.WriteLine("staffEmailValidation TRUE 02: e98765@rmit.edu.au " + staffEmailValidation("e98765@rmit.edu.au"));
+            Console.WriteLine("staffEmailValidation FALSE 01: e12345@student.rmit.edu.au " + staffEmailValidation("e12345@student.rmit.edu.au"));
+            Console.WriteLine("staffEmailValidation FALSE 02: s1234567@student.rmit.edu.au " + staffEmailValidation("s1234567@student.rmit.edu.au"));
+            Console.WriteLine("staffEmailValidation FALSE 03: e12345@gmail.com " + staffEmailValidation("e12345@gmail.com"));
+
+            //STUDENT EMAIL
+            Console.WriteLine("studentEmailValidation TRUE 01: s1234567@student.rmit.edu.au " + studentEmailValidation("s1234567@student.rmit.edu.au"));
+            Console.WriteLine("studentEmailValidation TRUE 02: s3530160@student.rmit.edu.au " + studentEmailValidation("s3530160@student.rmit.edu.au"));
+            Console.WriteLine("studentEmailValidation FALSE 01: e12345@rmit.edu.au " + studentEmailValidation("e12345@rmit.edu.au"));
+            Console.WriteLine("studentEmailValidation FALSE 02: s1234567@rmit.edu.au " + studentEmailValidation("s1234567@rmit.edu.au"));
+            Console.WriteLine("studentEmailValidation FALSE 03: e12345@gmail.com " + studentEmailValidation("e12345@gmail.com"));
+
+            Console.WriteLine("END OF DATA VALIDATION CHECKS");
+            Console.WriteLine("------------------------------------------------------------");
+            Console.ReadLine();
+        }
+
+        //The Staff ID always starts with a letter ‘e’ followed by 5 numbers.
+
+        //REGEX IS ^e+\d{5}$
+        public static Boolean staffIdValidation(string staffId)
+        {
+            string regexString = @"^e+\d{5}$";
+            return Regex.IsMatch(staffId, regexString);
+        }
+
+        //The Student ID always starts with a letter ‘s’ followed by 7 numbers.
+        //REGEX IS ^s+\d{7}$
+        public static Boolean studentIdValidation(string studentId)
+        {
+            string regexString = @"^s+\d{7}$";
+            return Regex.IsMatch(studentId, regexString);
+        }
+
+        //Email for a staff always ends with rmit.edu.au
+        //REGEX IS ^(e+\d{5})+@+rmit.edu.au$
+        public static Boolean staffEmailValidation(string staffEmail)
+        {
+            string regexString = @"^(e+\d{5})+@+rmit.edu.au$";
+            return Regex.IsMatch(staffEmail, regexString);
+        }
+
+
+        //Email for a student always ends with student.rmit.edu.au
+        //REGEX IS ^(s+\d{7})+@+student.rmit.edu.au$
+        public static Boolean studentEmailValidation(string studentEmail)
+        {
+            string regexString = @"^(s+\d{7})+@+student.rmit.edu.au$";
+            return Regex.IsMatch(studentEmail, regexString);
+        }
+
+        //Each ID (Staff and Student) is unique.
+        public static Boolean idUniqueCheck(string id)
+        {
+            return false;
         }
     }
 }
