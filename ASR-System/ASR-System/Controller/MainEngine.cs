af663df1b137c8252de2d3a547142bad68beb26f
Elliot Schot <s3530160@student.rmit.edu.au>
2019-01-13 11:00:41 +1100

    adding MainEngine

---
+++ ASR-System/ASR-System/Controller/MainEngine.cs
@@ -0,0 +1,24 @@
+﻿using ASR_System.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASR_System.Controller
+{
+    public class MainEngine
+    {
+        //public List<Staff> StaffList { get; }
+
+
+        public void ListStaff()
+        {
+            using (var connection = Program.ConnectionString.CreateConnection())
+            {
+                var command = connection.CreateCommand();
+                command.CommandText = "select * from dbo.[User] WHERE UserID LIKE 'e%'";
+
+                //StaffList = command.GetDataTable().Select().Select(x => new Inventory((int)x["InventoryID"], (string)x["Name"], (decimal)x["Price"])).ToList();
+            }
+        }
+    }
+}
