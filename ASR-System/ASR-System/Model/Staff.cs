58694121a922d78b10945ff4960db77a50b6d359
Elliot Schot <s3530160@student.rmit.edu.au>
2019-01-15 18:36:10 +1100

    removing whitespace

--- ASR-System/ASR-System/Model/Staff.cs
+++ ASR-System/ASR-System/Model/Staff.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace ASR_System.Model
 {
     public class Staff : User
     {
         public Staff(string userID, string name, string email) : base(userID, name, email) { }
+
     }
+
 
 }
